e11cb8a4be6a8196f601f96ddd5fde534f1adcbb
Enrique Moran <enrique.morango@udlap.mx>
2022-06-06 11:22:07 -0500

    Navbar in NextJs

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Enrique | Mechatronics Engineer</title>
+      </Head>
+      <Navbar />
+    </div>
+  );
+}
