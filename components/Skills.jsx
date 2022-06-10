7f9d421958f21a1a2df646cfb27c5f572e8d2efc
Enrique Moran <enrique.morango@udlap.mx>
2022-06-09 21:51:27 -0500

    keep trying 3

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,7 +14,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
               <div className="grid grid-cols-2 gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image
+                  <img
                     src="../public/assets/projects/python.png"
                     alt="/"
                     width={100}
