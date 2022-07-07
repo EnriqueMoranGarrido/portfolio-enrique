2b643615ca31fd86ec7c924da26f8b37cecca7a6
Enrique Moran <enrique.morango@udlap.mx>
2022-07-07 18:55:01 -0500

    created 2 components for skills, i deleted a bunch of code

---
+++ public/assets/skillsData.jsx
@@ -0,0 +1,82 @@
+export default [
+  {
+    id: "0",
+    title: "JavaScript",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/javascript.png",
+  },
+  {
+    id: "1",
+    title: "TypeScript",
+    link: "https://www.typescriptlang.org",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/typescript.png",
+  },
+  {
+    id: "2",
+    title: "HTML 5",
+    link: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",
+    image: "https://portfolio-enrique-images.s3.amazonaws.com/skills/html.png",
+  },
+  {
+    id: "3",
+    title: "CSS 3",
+    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+    image: "https://portfolio-enrique-images.s3.amazonaws.com/skills/css.png",
+  },
+  {
+    id: "4",
+    title: "TailwindCSS",
+    link: "https://tailwindcss.com",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/tailwind.png",
+  },
+  {
+    id: "5",
+    title: "React / React Native",
+    link: "https://en.reactjs.org",
+    image: "https://portfolio-enrique-images.s3.amazonaws.com/skills/react.png",
+  },
+  {
+    id: "6",
+    title: "Next JS",
+    link: "https://nextjs.org",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/nextjs.png",
+  },
+  {
+    id: "7",
+    title: "Expo",
+    link: "https://docs.expo.dev",
+    image: "https://portfolio-enrique-images.s3.amazonaws.com/skills/expo.png",
+  },
+  {
+    id: "8",
+    title: "AWS: Amplify",
+    link: "https://aws.amazon.com/amplify/",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/amplify.png",
+  },
+  {
+    id: "9",
+    title: "AWS: S3",
+    link: "https://aws.amazon.com/s3/?trk=5970b1e9-218b-48cc-9862-f23c151d81b2&sc_channel=ps&sc_campaign=acquisition&sc_medium=ACQ-P|PS-GO|Brand|Desktop|SU|Storage|S3|LATAMO|ES|Text&s_kwcid=AL!4422!3!590443989054!e!!g!!amazon%20s3&ef_id=Cj0KCQjwn4qWBhCvARIsAFNAMij5HINSEucAdVejWXZo1s8fuwFzkhzlbq22mh7Na0jzWJF3jW0df0UaArzlEALw_wcB:G:s&s_kwcid=AL!4422!3!590443989054!e!!g!!amazon%20s3",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/aws-s3-logo.png",
+  },
+  {
+    id: "10",
+    title: "Github",
+    link: "https://github.com",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/github1.png",
+  },
+  {
+    id: "11",
+    title: "Visual Studio Code",
+    link: "https://code.visualstudio.com",
+    image:
+      "https://portfolio-enrique-images.s3.amazonaws.com/skills/visualsc.png",
+  },
+];
